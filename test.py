c6736109fbfb5b6c837195b30477c9465c730221
Steini <hivsteini@gmail.com>
2016-12-09 18:57:07 +0000

    Sort orðið betra, tekur töluvert fleirri filea. Allir þættir sem hafa ekki nafn fara í sér möppu.

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 import os
 from guessit import guessit
 def test():
@@ -8,41 +9,61 @@ def test():
 ##                        , help='bla')
 ##    args = pareser.parse_args()
     
-    destination = ('C:\Verkefni4\Sorted')
-    downloads = ('C:\Verkefni4\downloads')
-    TvShow = ('C:\Verkefni4\Sorted\Tv-Show')
-    Movie = ('C:\Verkefni4\Sorted\Movies')
-##    others = ('C:\Verkefni4\Sorted\Unsorted')
+    destination = ('C:\Python-Verk4\Sorted')
+    downloads = ('C:\Python-Verk4\downloads')
+    TvShow = ('C:\Python-Verk4\Sorted\Tv-Show')
+    TvShowUnknown = ('C:\Python-Verk4\Sorted\Tv-Show-Unknown')
+    Movie = ('C:\Python-Verk4\Sorted\Movies')
     
-    vid_ext=('.mov','.mpg','.avi','.mp4','.wmv','.asf','.mpeg','.flv')
-
-
+    vid_ext = ('.mov','.mpg','.avi','.mp4','.wmv','.asf','.mpeg','.flv','mkv')
+    Rmv_files = ('.nfo', '.sfv', '.torrent', '.txt', '.mta', 'rar', '.part', '.dat', '.srt', '.DS_Store', '.lnk')
+    
     if not os.path.exists(destination):
-        os.makedirs(destination)
-            
+        os.makedirs(destination)            
             
     for root, dirs, files in os.walk(downloads):
         for f in files:
             if f.endswith(vid_ext):
                 guess = guessit(f)
-                if 'season' in guess:
+                if guess['type'] == 'episode':                 
                     if not os.path.exists(TvShow):
                         os.makedirs(TvShow)
-                    Season = str(guess['season'])
-                    if 'title' in guess:
-                        seasonName = str(guess['title'])
-                        
-                    showpath = os.path.join(TvShow, seasonName)
-                    CopyFile(showpath, Season, f, root)
-##                if not 'season' in guess or not 'episode_title' in guess:
-##                    if not os.path.exists(Movie):
-##                        os.makedirs(Movie)
-##                    movieName = str(guess['title'])
-##                    showpath = os.path.join(Movie, movieName)
-##                    CopyFile(Movie, movieName, f, root)
-
-                        
-def CopyFile(showpath, Season, f, root):
+                    if 'season' in guess and 'title'in guess:
+                        Season = str(guess['season'])
+                        SeasonName = str(guess['title'])
+                        showpath = os.path.join(TvShow, SeasonName)
+                        CopyTvShow(showpath, Season, f, root)
+                    elif 'season' in guess and not 'title' in guess:
+                        Season = str(guess['season'])
+                        SeasonName = 'Season-Name-Unknown'
+                        showpath = os.path.join(TvShowUnknown, SeasonName)
+                        CopyTvShow(showpath, Season, f, root)
+                    elif 'title' in guess and not 'season' in guess:
+                        Season = 'Unknown'
+                        SeasonName = str(guess['title'])
+                        showpath = os.path.join(TvShow, SeasonName)
+                        CopyTvShow(showpath, Season, f, root)
+                    else:
+                        Season = 'Season-Unknown'
+                        SeasonName = 'Season-Name-Unknown'
+                        showpath = os.path.join(TvShowUnknown, SeasonName)
+                        CopyTvShow(showpath, Season, f, root)                    
+                elif guess['type'] == 'movie' and guess['title'] != 'sample':
+                    if not os.path.exists(Movie):
+                        os.makedirs(Movie)
+                    movieName = str(guess['title'])
+                    showpath = os.path.join(Movie, movieName)
+                    CopyMovies(showpath, f, root)
+            elif f.endswith(Rmv_files):
+                os.remove(os.path.join(root, f))
+                
+        #Eftir eina keyrslu eru 680 folders eftir. En ef maður keyrir 2x þá eru ekki nema 250 folderar eftir
+        for d in dirs:    
+            if not os.listdir(os.path.join(root, d)):
+                os.removedirs(os.path.join(root, d))
+                       
+                
+def CopyTvShow(showpath, Season, f, root):
     if not os.path.exists(showpath):
          os.makedirs(showpath)
     if not os.path.exists(os.path.join(showpath, Season)):
@@ -53,17 +74,13 @@ def CopyFile(showpath, Season, f, root):
             os.rename(os.path.join(root, f), os.path.join(os.path.join(showpath, Season), f))
             print(os.path.join(os.path.join(showpath, Season), f))
 
-##def CopyFileMovies(showpath, Seas f, root):
-##    if not os.path.exists(showpath):
-##         os.makedirs(showpath)
-##    if not os.path.exists(os.path.join(showpath, Season)):
-##         os.makedirs(os.path.join(showpath, Season))
-##         os.rename(os.path.join(root, f), os.path.join(os.path.join(showpath, Season), f))
-##    else:
-##        if not os.path.exists(os.path.join(os.path.join(showpath, Season), f)):
-##            os.rename(os.path.join(root, f), os.path.join(os.path.join(showpath, Season), f))
-##            print(os.path.join(os.path.join(showpath, Season), f))    
-##                else:
-##                    if not os.path.exists(others):
-##                        os.makedirs(others)
-##                        os.rename(os.path.join(root, f), os.path.join(others, f))
+def CopyMovies(showpath, f, root):
+    if not os.path.exists(showpath):
+         os.makedirs(showpath)
+    if not os.path.exists(showpath):
+         os.makedirs(showpath)
+         os.rename(os.path.join(root, f), os.path.join(showpath, f))
+    else:
+        if not os.path.exists(os.path.join(showpath, f)):
+            os.rename(os.path.join(root, f), os.path.join(showpath, f))
+            print(os.path.join(showpath, f))
